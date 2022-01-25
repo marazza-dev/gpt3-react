9eba279a52892ae69c785c0d702f3d326661add8
Federico Marazzani <marazzani.dev@gmail.com>
2022-01-25 11:44:30 +0100

    implement Header and Brand components

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 import "./brand.css";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className='gpt3--brand section--padding'>
+      <div>
+        <img src={google} alt='Google Logo' />
+      </div>
+      <div>
+        <img src={slack} alt='Slack Logo' />
+      </div>
+      <div>
+        <img src={atlassian} alt='Atlassian Logo' />
+      </div>
+      <div>
+        <img src={dropbox} alt='Dropbox Logo' />
+      </div>
+      <div>
+        <img src={shopify} alt='Shopify Logo' />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
