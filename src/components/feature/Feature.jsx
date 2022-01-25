323de81ee4fa7a22f573cf430b8561527c297fef
Federico Marazzani <marazzani.dev@gmail.com>
2022-01-25 14:53:55 +0100

    implement Feature component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./feature.css";
 
-const Feature = () => {
-  return <div>Feature</div>;
+const Feature = ({ title, text }) => {
+  return (
+    <div className='gpt3--features-container-feature'>
+      <div className='gpt3--features-container-feature-title'>
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3--features-container-feature-text'>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./features.css";
 
+const featuresData = [
+  {
+    title: "Improving end distrusts instantly",
+    text: "",
+  },
+];
+
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className='gpt3--features section--padding' id='features'>
+      <div className='gpt3--features-heading'>
+        <h1 className='gradient--text'>
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className='gpt3--features-container'></div>
+    </div>
+  );
 };
 
 export default Features;
