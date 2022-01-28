75c33e7a4c97d037dbbe46899ba9ebbb8e51cb63
Federico Marazzani <marazzani.dev@gmail.com>
2022-01-28 13:22:16 +0100

    implement Footer component

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,51 @@
 import React from "react";
+import logo from "../../assets/logo.svg";
 import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='gpt3--footer'>
+      <div className='gpt3--footer-heading'>
+        <h1 className='gradient--text'>
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+
+      <div className='gpt3--footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+
+      <div className='gpt3--footer-links'>
+        <div className='gpt3--footer-links-logo'>
+          <img src={logo} alt='Brand Logo' />
+          <p>Crechterwoord K12 182 DK Alknjkcv, All Rights Reserved</p>
+        </div>
+        <div className='gpt3--footer-links-div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+
+        <div className='gpt3--footer-links-div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3--footer-links-div'>
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcv</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className='gpt3--footer-copyright'>
+        <p>@2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
